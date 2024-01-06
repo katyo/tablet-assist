831e9e1b60880e6b9fc3b6ffa9f9a13fb2df6e59
K. <kayo@illumium.org>
2024-01-06 15:02:31 +0500

    Use fixed zbus to represent enum types as strings

--- service/src/types.rs
+++ service/src/types.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
-use zbus::zvariant::{Error, OwnedValue, Result, Type, Value};
+use zbus::zvariant::{OwnedValue, Type, Value};
 
-#[derive(Debug, Clone, Copy, Default, Type, PartialEq, Serialize, Deserialize)]
-#[zvariant(signature = "s")]
+#[derive(
+    Debug, Clone, Copy, Default, PartialEq, Type, Value, OwnedValue, Serialize, Deserialize,
+)]
+#[zvariant(signature = "s", rename_all = "snake_case")]
 #[serde(rename_all = "kebab-case")]
 #[allow(clippy::enum_variant_names)]
 #[repr(u8)]
@@ -20,7 +22,10 @@ impl Orientation {
     }
 }
 
-#[derive(Debug, Clone, Copy, Default, Type, PartialEq, Serialize, Deserialize)]
+#[derive(
+    Debug, Clone, Copy, Default, PartialEq, Type, Value, OwnedValue, Serialize, Deserialize,
+)]
+#[zvariant(signature = "s", rename_all = "snake_case")]
 #[serde(rename_all = "kebab-case")]
 #[repr(u8)]
 pub enum OrientationType {
@@ -36,84 +41,5 @@ impl From<Orientation> for OrientationType {
         } else {
             Self::Portrait
         }
-    }
-}
-
-impl From<Orientation> for u8 {
-    fn from(orientation: Orientation) -> Self {
-        orientation as _
-    }
-}
-
-impl TryFrom<u8> for Orientation {
-    type Error = u8;
-    fn try_from(raw: u8) -> core::result::Result<Self, Self::Error> {
-        if raw >= Self::TopUp as _ && raw <= Self::BottomUp as _ {
-            Ok(unsafe { *(&raw as *const _ as *const _) })
-        } else {
-            Err(raw)
-        }
-    }
-}
-
-impl TryFrom<Value<'_>> for Orientation {
-    type Error = Error;
-
-    #[inline]
-    fn try_from(value: Value<'_>) -> Result<Self> {
-        let v = <&str>::try_from(&value)?;
-
-        Ok(match v {
-            "top-up" => Self::TopUp,
-            "left-up" => Self::LeftUp,
-            "right-up" => Self::RightUp,
-            "bottom-up" => Self::BottomUp,
-            _ => return Err(Error::IncorrectType),
-        })
-    }
-}
-
-impl From<Orientation> for Value<'_> {
-    #[inline]
-    fn from(e: Orientation) -> Self {
-        let u: &str = match e {
-            Orientation::TopUp => "top-up",
-            Orientation::LeftUp => "left-up",
-            Orientation::RightUp => "right-up",
-            Orientation::BottomUp => "bottom-up",
-        };
-
-        <Value as From<_>>::from(u).into()
-    }
-}
-
-impl TryFrom<OwnedValue> for Orientation {
-    type Error = Error;
-
-    #[inline]
-    fn try_from(value: OwnedValue) -> Result<Self> {
-        let v = <&str>::try_from(&value)?;
-
-        Ok(match v {
-            "top-up" => Self::TopUp,
-            "left-up" => Self::LeftUp,
-            "right-up" => Self::RightUp,
-            "bottom-up" => Self::BottomUp,
-            _ => return Err(Error::IncorrectType),
-        })
-    }
-}
-
-impl From<Orientation> for OwnedValue {
-    #[inline]
-    fn from(e: Orientation) -> Self {
-        let u: &str = match e {
-            Orientation::TopUp => "top-up",
-            Orientation::LeftUp => "left-up",
-            Orientation::RightUp => "right-up",
-            Orientation::BottomUp => "bottom-up",
-        };
-
-        <Value as From<_>>::from(u).into()
     }
 }
